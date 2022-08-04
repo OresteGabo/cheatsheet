9f3ac01219211615f01c29f444abae83c43f047e
oreste <muhirwa.g.oreste@gmail.com>
2022-08-04 17:55:54 +0200

    linking the head file to more stylesheets

--- private/shared/head.php
+++ private/shared/head.php
@@ -12,6 +12,14 @@ if(!isset($page_title)){
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="<?php echo url_for('/styles/style.css');?>">
     <link rel="stylesheet" href="<?php echo url_for('/styles/footer.css');?>">
+    <link rel="stylesheet" href="<?php echo url_for('/styles/accordion.css');?>">
+    <link rel="stylesheet" href="<?php echo url_for('/styles/cards.css');?>">
+    <link rel="stylesheet" href="<?php echo url_for('/styles/draft.css');?>">
+    <link rel="stylesheet" href="<?php echo url_for('/styles/forms.css');?>">
+    <link rel="stylesheet" href="<?php echo url_for('/styles/lists.css');?>">
+    <link rel="stylesheet" href="<?php echo url_for('/styles/tables.css');?>">
+    <link rel="stylesheet" href="<?php echo url_for('/styles/typography.css');?>">
+    <link rel="stylesheet" href="<?php echo url_for('/styles/code.css');?>">
     <title><?php echo $page_title; ?></title>
 
 
