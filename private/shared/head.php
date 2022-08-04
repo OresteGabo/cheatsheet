cbca6380f64f970f482104c29003652f2c1f0381
oreste <muhirwa.g.oreste@gmail.com>
2022-08-04 11:55:21 +0200

    update of href attributes path, that must be generated using php now, so it can work across different files

--- private/shared/head.php
+++ private/shared/head.php
@@ -10,7 +10,9 @@ if(!isset($page_title)){
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="styles/style.css">
-    <link rel="stylesheet" href="styles/footer.css">
+    <link rel="stylesheet" href="<?php echo url_for('/styles/style.css');?>">
+    <link rel="stylesheet" href="<?php echo url_for('/styles/footer.css');?>">
     <title><?php echo $page_title; ?></title>
+
+
 </head>
