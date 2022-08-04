f73223586516435832a9fb0f5a7f6f62d095154a
oreste <muhirwa.g.oreste@gmail.com>
2022-08-04 03:05:49 +0200

    css correction of some errors

--- pages/java/index.php
+++ pages/java/index.php
@@ -6,8 +6,79 @@ require_once('../page_parts/head.php');
 <body>
 <div class="container">
     <div class="container">
-        <aside><nav class="table-of-content"></nav></aside>
-        <main class="content"></main>
+      <aside   class="table-of-content">
+        <h3 class="title">La généricité en Java</h3>
+        <nav class=>
+          <ul>
+              <li><a href="">Introduction</a></li>
+              <li><a href="">Définition d’une classe générique</a></li>
+              <li><a href="">Restriction des types admis</a></li>
+              <li><a href="">Méthodes génériques</a>
+                <ul>
+                  <li><a href="">Sûreté du typage</a></li>
+                  <li><a href="">Inférence de type</a></li>
+                 
+                </ul>
+              </li>
+              <li><a href="">Le joker (wildcard)</a>
+                <ul>
+                  <li><a href="#">Le joker expliqué par l’exemple</a></li>
+                  <li><a href="#">L’utilisation du joker borné avec extends expliqué par
+l’exemple</a></li>
+                  <li><a href="#">L’utilisation du joker borné avec super expliqué par
+l’exemple</a></li>
+                  
+                  <li><a href="#"></a></li>
+                </ul>
+              </li>
+            <li><a href="#">Effacement</a></li>
+            <li><a href="#">Tableaux</a></li>
+            <li><a href="#">Exercices</a></li>
+          </ul>
+        </nav>
+
+      </aside>
+        <main class="content">
+          <section class="chapter">
+            <h1 class="title">Introduction</h1>
+            <div class="content">
+              <p>
+                Utiliser des classes génériques, comme on l’a vu dans un cours précédent, est
+relativement simple et recommandé, voire obligatoire.
+              </p>
+              <p>Créer des classes génériques est un peu plus compliqué. Cela nécessite de bien
+comprendre les mécanismes sous-jacents. C’est ce dont ce cours traite.</p>
+            </div>
+          </section>
+          <section class="chapter">
+            <h1 class="title">This is the First chapter title</h1>
+            <div class="content">
+              <p>La définition d’une classe générique peut se faire très simplement. Il suffit de
+faire suivre le nom de la classe par un nom de paramètre de type (terme consacré)
+ou même une liste de noms de paramètres de type entre « chevrons » ( <> ).</p>
+              <p>
+                L’exemple qui suit montre une classe dont les instances réunissent deux valeurs
+d’un même type, connu seulement au moment de la compilation.
+              </p>
+              <div class="code">
+<pre class="code">
+<code><span class="comment">// T est un paramètre (formel) de type.</span></code>
+<code><span class="comment">/* Dorénavant, un couple de n’importe quel type référence pourra être</span></code>
+<code><span class="comment">instancié. */</span></code>
+<code><span class="acsess-modifier">public</span> <span class="keyword">class</span> <span class="datatype">Couple</span><T> {</code>
+<code>	// Les attributs sont de type T.</code>
+<code>	<span class="acsess-modifier">public</span> <span class="keyword">final</span> <span class="datatype">T</span> <span class="property">un</span>, <span class="property">deux</span> ; <span class="comment">// Il n’est pas encore connu.</span></code>
+<code>	<span class="acsess-modifier">public</span> <span class="datatype">Couple</span>(<span class="datatype">T</span> <span class="property">un</span>, <span class="datatype">T</span> <span class="property">deux</span>) {</code>
+<code>		<span class="keyword">this</span>.<span class="property">un</span> = <span class="property">un</span> ; <span class="keyword">this</span>.<span class="property">deux</span> = <span class="property">deux</span> ;</code>
+<code>	}</code>
+<code>}</code>
+</pre>
+                        </div>
+                </div>
+          </section>
+            
+          
+        </main>
     </div>
 </div>
 </body>
