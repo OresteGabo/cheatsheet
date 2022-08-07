f0abf2c57e045499cbd8034450adba336795350a
oreste <muhirwa.g.oreste@gmail.com>
2022-08-07 02:54:22 +0200

    still debugging the images

--- private/shared/navigation/mainnav.php
+++ private/shared/navigation/mainnav.php
@@ -4,21 +4,21 @@ include('../../initialise.php');
 ?>
 
 <nav class="navbar">
-        <img src="<?php echo WWW_ROOT.'/logo-cropped.svg';?>" class="logo" alt="gabo logo">
-        <ul class="nav-menu">
-            <li class="nav-item">
-                <a href="#" class="nav-link">CSS</a>
-            </li>
-            <li class="nav-item active"><a href="#" class="nav-link " >Home</a></li>
-            <li class="nav-item"><a href="<?php echo WWW_ROOT.'/pages/java/index.php';?>" class="nav-link">Java</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Database</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">private</a></li>
-        </ul>
-        <div class="hamburger">
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
-        </div>
-    </nav>
+    <img src="<?php echo WWW_ROOT.'/logo-cropped.svg';?>" class="logo" alt="gabo logo">
+    <ul class="nav-menu">
+        <li class="nav-item">
+            <a href="#" class="nav-link">CSS</a>
+        </li>
+        <li class="nav-item active"><a href="#" class="nav-link " >Home</a></li>
+        <li class="nav-item"><a href="<?php echo WWW_ROOT.'/pages/java/index.php';?>" class="nav-link">Courses</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">private</a></li>
+    </ul>
+    <div class="hamburger">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </div>
+</nav>
 
  
