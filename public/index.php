3d1566750102e6e00f4b29a5bdc106bec8b0a775
oreste <muhirwa.g.oreste@gmail.com>
2022-08-13 16:34:33 +0200

    Update of pre codes

--- public/index.php
+++ public/index.php
@@ -47,31 +47,31 @@ require_once(SHARED_PATH.'/head.php');
                 <div class="code">
                 
                     <pre class="code">
-<code><span class="comment">//Padding widget is just the same as web padding</span></code>
-<code><span class="datatype">Padding</span>(</code>
-<code>	<span class="property">padding</span>: <span class="keyword">const</span> <span class="datatype">EdgeInsets</span>.symmetric(</code>
-<code>	  <span class="property">horizontal</span>: 25,</code>
-<code>	),</code>
-<code>	<span class="property">child</span>: <span class="datatype">Container</span>(</code>
-<code>	  <span class="property">decoration</span>: <span class="datatype">BoxDecoration</span>(</code>
-<code>	    <span class="property">color</span>: <span class="datatype">Colors</span>.deepPurple,</code>
-<code>	    <span class="property">borderRadius</span>: <span class="datatype">BorderRadius</span>.circular(12.0),</code>
-<code>	  ),</code>
-<code>	  <span class="property">child</span>: <span class="datatype">Padding</span>(</code>
-<code>	    <span class="property">padding</span>: <span class="keyword">const</span> <span class="datatype">EdgeInsets</span>.all(18.0),</code>
-<code>	    <span class="property">child</span>: <span class="datatype">Center</span>(</code>
-<code>	      <span class="property">child</span>: <span class="datatype">Text</span>(</code>
-<code>		'<span class="value string">Login</span>',</code>
-<code>		<span class="property">style</span>: <span class="datatype">TextStyle</span>(</code>
-<code>		  <span class="property">color</span>: <span class="datatype">Colors</span>.white,</code>
-<code>		  <span class="property">fontSize</span>: 15,</code>
-<code>		  <span class="property">fontWeight</span>: <span class="datatype">FontWeight</span>.bold,</code>
-<code>		),</code>
-<code>	      ),</code>
-<code>	    ),</code>
-<code>	  ),</code>
-<code>	),</code>
-<code>),</code></pre>
+<code>  <span class="comment">//Padding widget is just the same as web padding</span></code>
+<code>  <span class="datatype">Padding</span>(</code>
+<code>	    <span class="property">padding</span>: <span class="keyword">const</span> <span class="datatype">EdgeInsets</span>.symmetric(</code>
+<code>  	  <span class="property">horizontal</span>: 25,</code>
+<code>  	),</code>
+<code>  	<span class="property">child</span>: <span class="datatype">Container</span>(</code>
+<code>  	  <span class="property">decoration</span>: <span class="datatype">BoxDecoration</span>(</code>
+<code>  	    <span class="property">color</span>: <span class="datatype">Colors</span>.deepPurple,</code>
+<code>  	    <span class="property">borderRadius</span>: <span class="datatype">BorderRadius</span>.circular(12.0),</code>
+<code>  	  ),</code>
+<code>  	  <span class="property">child</span>: <span class="datatype">Padding</span>(</code>
+<code>  	    <span class="property">padding</span>: <span class="keyword">const</span> <span class="datatype">EdgeInsets</span>.all(18.0),</code>
+<code>  	    <span class="property">child</span>: <span class="datatype">Center</span>(</code>
+<code>  	      <span class="property">child</span>: <span class="datatype">Text</span>(</code>
+<code>  		'<span class="value string">Login</span>',</code>
+<code>  		<span class="property">style</span>: <span class="datatype">TextStyle</span>(</code>
+<code>  		  <span class="property">color</span>: <span class="datatype">Colors</span>.white,</code>
+<code>  		  <span class="property">fontSize</span>: 15,</code>
+<code>  		  <span class="property">fontWeight</span>: <span class="datatype">FontWeight</span>.bold,</code>
+<code>       		),</code>
+<code>  	      ),</code>
+<code>  	    ),</code>
+<code>  	  ),</code>
+<code>  	),</code>
+<code>  ),</code></pre>
                     <p class="code-title">Padding example code sample in flutter   </p>
                 </div>
 
